a079c1623a475e68f3117aad9c91b244e6046d12
neyla bouebdellah <neylabouebdellah@gmail.com>
2021-06-07 16:18:51 +0100

    msfg

--- servomo.py
+++ servomo.py
@@ -10,22 +10,15 @@ p.start(2.5) # Initialization
 
 try:
   while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
     p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
+    Time.Sleep(10)
     p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
+    p.stop()
+    GPIO.cleanup()
+
+
+
+
 except KeyboardInterrupt:
   p.stop()
 
